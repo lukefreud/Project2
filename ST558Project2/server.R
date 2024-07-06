631f1d5e93918c12acf30bbb696cdcee7eacdc9a
lukefreud <lukefreud@gmail.com>
2024-07-06 10:40:07 -0400

    Starting column filters by user and making function for URL

--- ST558Project2/server.R
+++ ST558Project2/server.R
@@ -7,15 +7,18 @@ data("GermanCredit")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  
+  API_Cleaning <- function(url) {
+    raw_data <- httr::GET(url)
+    parsed_data <- fromJSON(rawToChar(raw_data$content))
+    final_data <- as_tibble(parsed_data$data)
+    return(final_data)
+  }
   output$summary <- DT::renderDataTable({
     if (input$var == "Gold Reserve") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/gold_reserve"
-      raw_data <- httr::GET(url)
-      parsed_data <- fromJSON(rawToChar(raw_data$content))
-      final_data <- as_tibble(parsed_data$data)
+      final_data <- API_Cleaning(url)
       final_data <- final_data |>
-        select(record_date, facility_desc, form_desc, location_desc, fine_troy_ounce_qty, book_value_amt, record_calendar_year)
+        select(all_of(input$GRColumns))
         if(input$GRFilter == "both") {
           final_data <- final_data
         }
@@ -30,9 +33,7 @@ shinyServer(function(input, output) {
       return(final_data)
     } else if (input$var == "Balance Sheets") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/balance_sheets"
-      raw_data <- httr::GET(url)
-      parsed_data <- fromJSON(rawToChar(raw_data$content))
-      final_data <- as_tibble(parsed_data$data)
+      final_data <- API_Cleaning(url)
       final_data <- final_data |>
         select(record_date, restmt_flag, account_desc, line_item_desc, position_bil_amt, record_calendar_year)
       if(input$BSFilter == "all") {
@@ -53,19 +54,15 @@ shinyServer(function(input, output) {
       return(final_data)
     } else if (input$var == "Interest Rates") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/avg_interest_rates"
-      raw_data <- httr::GET(url)
-      parsed_data <- fromJSON(rawToChar(raw_data$content))
-      final_data <- as_tibble(parsed_data$data)
+      final_data <- API_Cleaning(url)
       final_data <- final_data |>
         select(record_date, security_type_desc, security_desc, avg_interest_rate_amt, record_calendar_year)
       return(final_data)
     } else if (input$var == "Electronic Securities") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/securities_redemptions"
-      raw_data <- httr::GET(url)
-      parsed_data <- fromJSON(rawToChar(raw_data$content))
-      final_data <- as_tibble(parsed_data$data)
+      final_data <- API_Cleaning(url)
       final_data <- final_data |>
-        select(record_date, security_type_desc, security_class_desc, securities_redeemed_cnt, securities_redeemed_amt, record_calendar_year)
+        select(all_of(input$ESColumns))
       if(input$ESFilter == "both") {
         final_data <- final_data
       }
@@ -80,17 +77,13 @@ shinyServer(function(input, output) {
       return(final_data)
     } else if (input$var == "Public Debt") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/debt/mspd/mspd_table_1"
-      raw_data <- httr::GET(url)
-      parsed_data <- fromJSON(rawToChar(raw_data$content))
-      final_data <- as_tibble(parsed_data$data)
+      final_data <- API_Cleaning(url)
       final_data <- final_data |>
         select(record_date, security_type_desc, security_class_desc, debt_held_public_mil_amt, intragov_hold_mil_amt, total_mil_amt, record_calendar_year)
       return(final_data)
     } else if (input$var == "Bond Issues") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/savings_bonds_report"
-      raw_data <- httr::GET(url)
-      parsed_data <- fromJSON(rawToChar(raw_data$content))
-      final_data <- as_tibble(parsed_data$data)
+      final_data <- API_Cleaning(url)
       final_data <- final_data |>
         select(record_date, series_cd, series_desc, bonds_issued_cnt, bonds_redeemed_cnt, bonds_out_cnt, bonds_matured_cnt, bonds_unmatured_cnt, record_calendar_year)
       return(final_data)
--- ST558Project2/ui.R
+++ ST558Project2/ui.R
@@ -24,13 +24,20 @@ shinyUI(fluidPage(
                   selected = "Gold Reserve"),
       conditionalPanel(
         "input.var == 'Gold Reserve'",
-        radioButtons("GRFilter", "Filter By Date", choices = c("2012","2013", "both"), selected = "both")),
+        radioButtons("GRFilter", "Filter By Date", choices = c("2012","2013", "both"), selected = "both"),
+        checkboxGroupInput("GRColumns", "Select desired columns", choices = 
+                            c("record_date", "facility_desc", "form_desc", "location_desc", "fine_troy_ounce_qty", "book_value_amt", "record_calendar_year"), 
+                            selected = c("record_date", "facility_desc", "form_desc", "location_desc", "fine_troy_ounce_qty", "book_value_amt", "record_calendar_year"))),
+    
       conditionalPanel(
         "input.var == 'Balance Sheets'",
         radioButtons("BSFilter", "Filter By Date", choices = c("1995", "1996", "1997", "all"), selected = "all")),
       conditionalPanel(
         "input.var == 'Electronic Securities'",
-        radioButtons("ESFilter", "Filter By Date", choices = c("2002", "2003", "both"), selected = "both")),
+        radioButtons("ESFilter", "Filter By Date", choices = c("2002", "2003", "both"), selected = "both"),
+        checkboxGroupInput("ESColumns", "Select desired columns", choices = 
+                             c("record_date", "security_type_desc", "security_class_desc", "securities_redeemed_cnt", "securities_redeemed_amt", "record_calendar_year"), 
+                           selected = c("record_date", "security_type_desc", "security_class_desc", "securities_redeemed_cnt", "securities_redeemed_amt", "record_calendar_year"))),
       numericInput("round", "Select the number of digits for rounding", value = 2, min = 0, max = 5)
     ),
     
