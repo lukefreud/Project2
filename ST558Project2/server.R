5832996e16f026259351792b97e55498070b26c2
lukefreud <lukefreud@gmail.com>
2024-07-07 13:25:08 -0400

    Updating labels of variables and starting exploration tab

--- ST558Project2/server.R
+++ ST558Project2/server.R
@@ -2,113 +2,113 @@ library(shiny)
 library(caret)
 library(tidyverse)
 library(DT)
-data("GermanCredit")
 
-
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  # Function to clean each URL
   API_Cleaning <- function(url) {
     raw_data <- httr::GET(url)
     parsed_data <- fromJSON(rawToChar(raw_data$content))
     final_data <- as_tibble(parsed_data$data)
     return(final_data)
   }
-  output$summary <- DT::renderDataTable({
+  
+  
+  
+  data <- reactive(
     if (input$var == "Gold Reserve") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/gold_reserve"
       final_data <- API_Cleaning(url)
       final_data <- final_data |>
-        select(all_of(input$GRColumns))
-        if(input$GRFilter == "both") {
-          final_data <- final_data
-        }
-        if(input$GRFilter == "2012") {
-          final_data <- final_data |>
-            filter(record_calendar_year == "2012")
-        }
-        if (input$GRFilter == "2013") {
-          final_data <- final_data |>
-            filter(record_calendar_year == "2013")
-        }
-      return(final_data)
+        select(all_of(input$GRColumns), record_calendar_year)
+      if(input$GRFilter == "both") {
+        final_data_GR <- final_data
+      }
+      if(input$GRFilter == "2012") {
+        final_data_GR <- final_data |>
+          filter(record_calendar_year == "2012")
+      }
+      if (input$GRFilter == "2013") {
+        final_data_GR <- final_data |>
+          filter(record_calendar_year == "2013")
+      }
+      return(final_data_GR)
     } else if (input$var == "Balance Sheets") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/balance_sheets"
       final_data <- API_Cleaning(url)
       final_data <- final_data |>
-        select(all_of(input$BSColumns))
+        select(all_of(input$BSColumns), record_calendar_year)
       if(input$BSFilter == "all") {
-        final_data <- final_data
+        final_data_BS <- final_data
       }
       if(input$BSFilter == "1995") {
-        final_data <- final_data |>
+        final_data_BS <- final_data |>
           filter(record_calendar_year == "1995")
       }
       if (input$BSFilter == "1996") {
-        final_data <- final_data |>
+        final_data_BS <- final_data |>
           filter(record_calendar_year == "1996")
       }
       if (input$BSFilter == "1997") {
-        final_data <- final_data |>
+        final_data_BS <- final_data |>
           filter(record_calendar_year == "1997")
       }
       return(final_data)
     } else if (input$var == "Interest Rates") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/avg_interest_rates"
       final_data <- API_Cleaning(url)
-      final_data <- final_data |>
-        select(all_of(input$IRColumns))
-      return(final_data)
+      final_data_IR <- final_data |>
+        select(all_of(input$IRColumns), record_calendar_year)
+      return(final_data_IR)
     } else if (input$var == "Electronic Securities") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/securities_redemptions"
       final_data <- API_Cleaning(url)
       final_data <- final_data |>
-        select(all_of(input$ESColumns))
+        select(all_of(input$ESColumns), record_calendar_year)
       if(input$ESFilter == "both") {
-        final_data <- final_data
+        final_data_ES <- final_data
       }
       if(input$ESFilter == "2002") {
-        final_data <- final_data |>
+        final_data_ES <- final_data |>
           filter(record_calendar_year == "2002")
       }
       if (input$ESFilter == "2003") {
-        final_data <- final_data |>
+        final_data_ES <- final_data |>
           filter(record_calendar_year == "2003")
       }
-      return(final_data)
+      return(final_data_ES)
     } else if (input$var == "Public Debt") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/debt/mspd/mspd_table_1"
       final_data <- API_Cleaning(url)
-      final_data <- final_data |>
-        select(all_of(input$PDColumns))
-      return(final_data)
+      final_data_PD <- final_data |>
+        select(all_of(input$PDColumns), record_calendar_year)
+      return(final_data_PD)
     } else if (input$var == "Bond Issues") {
       url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/savings_bonds_report"
       final_data <- API_Cleaning(url)
-      final_data <- final_data |>
-        select(all_of(input$BIColumns))
-      return(final_data)
+      final_data_BI <- final_data |>
+        select(all_of(input$BIColumns), record_calendar_year)
+      return(final_data_BI)
     }
-    round <- input$round
-    tab <- GermanCredit %>% 
-      select("Class", "InstallmentRatePercentage", var) %>%
-      group_by(Class, InstallmentRatePercentage) %>%
-      summarize(mean = round(mean(get(var)), round))
-    tab
+  )
+  output$summary <- DT::renderDataTable({
+    data()
   })
   
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      'US-Treasury-Data.csv'
+    },
+    content = function(file) {
+      write.csv(data(), file, row.names = FALSE)
+    }
+  )
+  output$contingencyTable <- renderTable({
+    url <- "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/gold_reserve"
+    final_data_GR <- API_Cleaning(url)
+    selected_data <- final_data_GR |>
+      select(all_of(input$CategoricalSummaries))
+    table(selected_data)
+  })
   output$barPlot <- renderPlot({
-    
-    g <- ggplot(GermanCredit, aes(x = Class))  
-    
-    if(input$plot == "bar"){
-      g + geom_bar()
-    } else if(input$plot == "sideUmemploy"){ 
-      g + geom_bar(aes(fill = as.factor(EmploymentDuration.Unemployed)), position = "dodge") + 
-        scale_fill_discrete(name = "Unemployment status", labels = c("Employed", "Unemployed"))
-    } else if(input$plot == "sideForeign"){
-      g + geom_bar(aes(fill = as.factor(ForeignWorker)), position = "dodge") + 
-        scale_fill_discrete(name = "Status", labels = c("German", "Foreign"))
-    }
-  })
-  
+})
 })
--- ST558Project2/ui.R
+++ ST558Project2/ui.R
@@ -1,7 +1,8 @@
 library(shiny)
 library(DT)
 library(caret)
-data("GermanCredit")
+library(httr)
+library(dplyr)
 
 
 # Define UI for application that draws a histogram
@@ -9,61 +10,103 @@ shinyUI(fluidPage(
   
   # Application title
   titlePanel("Summaries for US Treasury Data"),
+  tabsetPanel(
   
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
+  tabPanel("About",
+           mainPanel(
+             h3("Purpose of the App"),
+             p("The purpose of this app is to allow the user to explore different datasets given by the US
+               Treasury API. The datasets include the balance sheets of the treasury, the interest rates set,
+               the public debt amounts, and the amount stored in gold reserves at certain time periods. The
+               user can view the datasets, subset which columns they want, filter certain years, and then download
+               whatever subset of the datasets they would like. The user can then choose which variables they would
+               like to be summarized and which graphs they want within the dataset."),
+             h3("Source of the Data"),
+             p("This data comes from an official website of the US government, FiscalData. The link for more
+               information on the API and documentation can be found here:", a(href =  "https://fiscaldata.treasury.gov/api-documentation/", "US Treasury API")),
+             h3("Purpose of each Tab"),
+             p("The purpose of the 'About' tab is to give the user an overview of this app, the API that the data sets
+               come from, and a link to this API. The purpose of the 'Data Download' tab is to allow the user to query to
+               a certain API endpoint, filter the data set by year, and select certain columns of it. The user is then able
+               to download whatever subset of the data set they would like. Lastly, the purpose of the 'Data Exploration' tab
+               is to allow the user to access different numerical summaries of certain variables within the data set. Also,
+               in this tab the user can select from options of graphical summaries to present the chosen parameters in the data."),
+           ),
+           sidebarPanel(tags$img(src = 'us-treasury.png', align = "center", width = "265px", length = "265px"))
+           ),
+  # Data Download tab panel
+  tabPanel("Data Download",
+    sidebarLayout(
     sidebarPanel(
       h3("This data set comes from the API of US Treasury data."),
       br(),
-      h4("You can select a dataset using the buttons below."),
-      radioButtons("plot", "Select the Plot Type", choices = list("Just Classification" = "bar", "Classification and Unemployed" = "sideUmemploy", "Classification and Foreign" = "sideForeign"), selected = "bar"),
-      br(),
-      h4("You can find the", strong("sample mean"), " for a few variables below:"),
       selectInput("var", label = "Dataset to choose", 
                   choices = c("Gold Reserve", "Balance Sheets", "Interest Rates", "Electronic Securities", "Public Debt", "Bond Issues"),
                   selected = "Gold Reserve"),
       conditionalPanel(
         "input.var == 'Gold Reserve'",
-        radioButtons("GRFilter", "Filter By Date", choices = c("2012","2013", "both"), selected = "both"),
+        radioButtons("GRFilter", "Filter By Year", choices = c("2012","2013", "both"), selected = "both"),
         checkboxGroupInput("GRColumns", "Select desired columns", choices = 
-                            c("record_date", "facility_desc", "form_desc", "location_desc", "fine_troy_ounce_qty", "book_value_amt", "record_calendar_year"), 
-                            selected = c("record_date", "facility_desc", "form_desc", "location_desc", "fine_troy_ounce_qty", "book_value_amt", "record_calendar_year"))),
+                            c("Record Date" = "record_date","Facility" = "facility_desc","Form of Gold" = "form_desc","Location" = "location_desc", 
+                              "Gold Quantity (Ounces)" = "fine_troy_ounce_qty","Book Value" = "book_value_amt"), 
+                            selected = c("record_date", "facility_desc", "form_desc", "location_desc", "fine_troy_ounce_qty", "book_value_amt"))),
     
       conditionalPanel(
         "input.var == 'Balance Sheets'",
-        radioButtons("BSFilter", "Filter By Date", choices = c("1995", "1996", "1997", "all"), selected = "all"),
+        radioButtons("BSFilter", "Filter By Year", choices = c("1995", "1996", "1997", "all"), selected = "all"),
         checkboxGroupInput("BSColumns", "Select desired columns", choices = 
-                             c("record_date", "restmt_flag", "account_desc", "line_item_desc", "position_bil_amt", "record_calendar_year"),
-                           selected = c("record_date", "restmt_flag", "account_desc", "line_item_desc", "position_bil_amt", "record_calendar_year"))),
+                             list("Record Date" = "record_date","Reocurring Charge?" = "restmt_flag","Account" = "account_desc", 
+                              "Line Item" = "line_item_desc","Bill Amount" = "position_bil_amt"),
+                           selected = list("Record Date" = "record_date","Restatement Variable" = "restmt_flag","Account" = "account_desc", 
+                                           "Line Item" = "line_item_desc","Bill Amount" = "position_bil_amt"))),
       conditionalPanel(
         "input.var == 'Interest Rates'",
         checkboxGroupInput("IRColumns", "Select desired columns", choices =
-                             c("record_date", "security_type_desc", "security_desc", "avg_interest_rate_amt", "record_calendar_year"),
-                           selected = c("record_date", "security_type_desc", "security_desc", "avg_interest_rate_amt", "record_calendar_year"))),
+                             list("Record Date" = "record_date","Security Type" = "security_type_desc","Specific Security" = "security_desc", 
+                                  "Average Interest Rate" = "avg_interest_rate_amt"),
+                           selected = list("Record Date" = "record_date","Security Type" = "security_type_desc","Specific Security" = "security_desc", 
+                                           "Average Interest Rate" = "avg_interest_rate_amt"))),
       conditionalPanel(
         "input.var == 'Electronic Securities'",
-        radioButtons("ESFilter", "Filter By Date", choices = c("2002", "2003", "both"), selected = "both"),
+        radioButtons("ESFilter", "Filter By Year", choices = c("2002", "2003", "both"), selected = "both"),
         checkboxGroupInput("ESColumns", "Select desired columns", choices = 
-                             c("record_date", "security_type_desc", "security_class_desc", "securities_redeemed_cnt", "securities_redeemed_amt", "record_calendar_year"), 
-                           selected = c("record_date", "security_type_desc", "security_class_desc", "securities_redeemed_cnt", "securities_redeemed_amt", "record_calendar_year"))),
+                             list("Record Date" = "record_date","Security Type" = "security_type_desc", "Security Class" = "security_class_desc", 
+                                  "Securities Redeemed" = "securities_redeemed_cnt","Value of Securities Redeemed" = "securities_redeemed_amt"), 
+                           selected = list("Record Date" = "record_date","Security Type" = "security_type_desc", "Security Class" = "security_class_desc", 
+                                           "Securities Redeemed" = "securities_redeemed_cnt","Value of Securities Redeemed" = "securities_redeemed_amt"))),
     conditionalPanel(
       "input.var == 'Public Debt'",
       checkboxGroupInput("PDColumns", "Select desired columns", choices =
-                           c("record_date", "security_type_desc", "security_class_desc", "debt_held_public_mil_amt", "intragov_hold_mil_amt", "total_mil_amt", "record_calendar_year"),
-                         selected = c("record_date", "security_type_desc", "security_class_desc", "debt_held_public_mil_amt", "intragov_hold_mil_amt", "total_mil_amt", "record_calendar_year"))),
+                           list("Record Date" = "record_date","Security Type" = "security_type_desc","Security Class" = "security_class_desc","Public Debt Held (in Millions)" = "debt_held_public_mil_amt",
+                             "Intra-Government Debt Held (in Millions)" = "intragov_hold_mil_amt","Total Debt Held (in Millions)" = "total_mil_amt"),
+                         selected = list("Record Date" = "record_date","Security Type" = "security_type_desc","Security Class" = "security_class_desc","Public Debt Held (in Millions)" = "debt_held_public_mil_amt",
+                                         "Intra-Government Debt Held (in Millions)" = "intragov_hold_mil_amt","Total Debt Held (in Millions)" = "total_mil_amt"))),
     conditionalPanel(
       "input.var == 'Bond Issues'",
       checkboxGroupInput("BIColumns", "Select desired columns", choices =
-                           c("record_date", "series_cd", "series_desc", "bonds_issued_cnt", "bonds_redeemed_cnt", "bonds_out_cnt", "bonds_matured_cnt", "bonds_unmatured_cnt", "record_calendar_year"),
-                         selected = c("record_date", "series_cd", "series_desc", "bonds_issued_cnt", "bonds_redeemed_cnt", "bonds_out_cnt", "bonds_matured_cnt", "bonds_unmatured_cnt", "record_calendar_year"))),
-      numericInput("round", "Select the number of digits for rounding", value = 2, min = 0, max = 5)
+                           list("Record Date" = "record_date","CD Series" = "series_cd", "Overall Series" = "series_desc", "Bonds Issued" = "bonds_issued_cnt",
+                              "Bonds Redeemed" =  "bonds_redeemed_cnt","Bonds Outstanding" = "bonds_out_cnt", "Bonds Matured" = "bonds_matured_cnt", "Bonds Unmatured" = "bonds_unmatured_cnt"),
+                         selected = list("Record Date" = "record_date","CD Series" = "series_cd", "Overall Series" = "series_desc", "Bonds Issued" = "bonds_issued_cnt",
+                                         "Bonds Redeemed" =  "bonds_redeemed_cnt","Bonds Outstanding" = "bonds_out_cnt", "Bonds Matured" = "bonds_matured_cnt", "Bonds Unmatured" = "bonds_unmatured_cnt"))),
+    downloadButton("downloadData", "Download CSV")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("barPlot"),
-      br(),
       dataTableOutput("summary")
     )
+  )
+  ),
+  tabPanel("Data Exploration",
+           sidebarLayout(sidebarPanel(
+      checkboxGroupInput("CategoricalSummaries", "Select variables to make a contigency table for.",
+                         choices = list("Facility" = "facility_desc", 
+                                        "Form of Gold" = "form_desc", "Location" = "location_desc"),
+                         selected = list("Facility" = "facility_desc"))
+           ),
+      mainPanel(tableOutput("contingencyTable"))
+           )
+      
+  )
   )
 ))
